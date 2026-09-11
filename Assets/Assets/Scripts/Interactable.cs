8e1733db45f06256301e5cf5c35656c264ce98e1
Neowb <stephenjohngavaran22@gmail.com>
2026-09-12 04:19:09 +0800

    Added door animation to all of the glass doors

--- Assets/Assets/Scripts/Interactable.cs
+++ Assets/Assets/Scripts/Interactable.cs
@@ -1,10 +1,27 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Interactable : MonoBehaviour
 {
     [SerializeField] private Transform uiAnchor;
     [SerializeField] private string promptText = "[E] Open";
 
+    [Header("Interaction")]
+    [SerializeField] private UnityEvent onInteract = new UnityEvent();
+
     public Transform UIAnchor => uiAnchor != null ? uiAnchor : transform;
     public string PromptText => promptText;
+
+    public void Interact()
+    {
+        if (isActiveAndEnabled)
+        {
+            onInteract.Invoke();
+        }
+    }
+
+    public void SetPromptText(string text)
+    {
+        promptText = text;
+    }
 }
