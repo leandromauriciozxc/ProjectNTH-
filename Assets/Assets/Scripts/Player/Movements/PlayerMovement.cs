c1b623d7538486a0b29a207fd223d10c2583af6b
Metoku <tommipallarco@gmail.com>
2026-09-26 23:16:58 +0800

    [*] modified playermovement script and added a Footstep Surface Reader Script to the player. and added surfaces for the outdoor scene

--- Assets/Assets/Scripts/Player/Movements/PlayerMovement.cs
+++ Assets/Assets/Scripts/Player/Movements/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using FSR;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
@@ -7,11 +8,15 @@ public class PlayerMovement : MonoBehaviour
     public float walkSpeed = 3.5f;
     public float runSpeed = 5.5f;
     public float gravity = -9.81f;
+    public float walkStepInterval = 0.5f;
+    public float runStepInterval = 0.3f;
 
+    float stepTimer;
     CharacterController controller;
     InputReader input;
 
     Vector3 velocity;
+    FSR_Player footstepAudioHandler;
 
     public Vector3 MoveDirection { get; private set; }
     public bool IsRunning { get; private set; }
@@ -20,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         input = GetComponent<InputReader>();
+        footstepAudioHandler = GetComponent<FSR_Player>();
     }
 
     void Update()
@@ -39,8 +45,29 @@ public class PlayerMovement : MonoBehaviour
         MoveDirection = transform.right * move.x + transform.forward * move.y;
 
         controller.Move(MoveDirection * speed * Time.deltaTime);
-    }
 
+        HandleFootsteps(move);
+    }
+    void HandleFootsteps(Vector2 move)
+    {
+        //if (!controller.isGrounded)
+        //    return;
+
+        if (move.sqrMagnitude < 0.01f)
+        {
+            stepTimer = 0f;
+            return;
+        }
+
+        stepTimer -= Time.deltaTime;
+
+        if (stepTimer <= 0f)
+        {
+            footstepAudioHandler.step();
+
+            stepTimer = IsRunning ? runStepInterval : walkStepInterval;
+        }
+    }
     void ApplyGravity()
     {
         if (controller.isGrounded && velocity.y < 0)
